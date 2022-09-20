a39d4fe73c016e4324143c537630484a486a5253
SARIT NGAMSANGIAM <59050316@kmitl.ac.th>
2022-09-20 21:35:59 +0700

    add data rule

---
+++ rules/pythonAddtag.py
@@ -0,0 +1,20 @@
+from audioop import add
+import re
+
+fileread = open('./exploitRules.rules','r')
+filewrite = open('./exploitewithtag.rules','a')
+
+
+for i in fileread:
+    # line = fileread.readline(i)
+    # print(i)
+    reg = re.search('.*msg\:\"(?P<msg>.*?)\"',i)
+    if reg:
+        cutdata = re.search('(?P<data>.*?)\)',i)
+        if cutdata:
+            data = cutdata['data']
+            msg =  reg['msg'].replace(" ", "_")
+            addtag = data+"metadata: tag " + msg + ";)"
+            fulladdtag = (f'{addtag}\n')
+            filewrite.write(fulladdtag)
+            print(addtag)
